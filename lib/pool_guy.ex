0cf9086a804117d7f913ad810199f8f48be8d88d
Tai An Su <taiansu@gmail.com>
2018-11-01 04:32:34 +0800

    Let's start from here

---
+++ lib/pool_guy.ex
@@ -0,0 +1,18 @@
+defmodule PoolGuy do
+  @moduledoc """
+  Documentation for PoolGuy.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PoolGuy.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pool_guy_test.exs
@@ -0,0 +1,8 @@
+defmodule PoolGuyTest do
+  use ExUnit.Case
+  doctest PoolGuy
+
+  test "greets the world" do
+    assert PoolGuy.hello() == :world
+  end
+end
